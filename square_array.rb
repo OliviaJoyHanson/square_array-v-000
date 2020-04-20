2fc137140cbc85ddecf5d55d37d8a9f48f89149b
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-20 14:15:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
-def square_array(array)
+#def square_array(array)
   # your code here
-  new_array = []
-  array.each do |index|
-    new_array << index*index
-  end
-  new_array
-end
+#  new_array = []
+#  array.each do |index|
+#    new_array << index*index
+#  end
+#  new_array
+#end
 
 def square_array(array)
   array.collect{|index| index*index}
